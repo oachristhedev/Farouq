158e06d9938f7b2e014590d7ea9fdf8b66d79639
oachristhedev <oachristhedev.@gmail.com>
2026-06-04 15:09:31 +0000

    return and functions

---
+++ proj-05/script.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+  return a + b;
+
+  console.log("Whatever you put after return never runs");
+}
+
+const value = sum(1, 2);
+
+console.log(value);
+
+// "Console.log()" is a function
